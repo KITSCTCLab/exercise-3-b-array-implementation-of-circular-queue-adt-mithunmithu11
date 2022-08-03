81299bf2ff1c5e6954029e4c537f8103f94bbe40
mithunmithu11 <110010668+mithunmithu11@users.noreply.github.com>
2022-08-03 09:43:27 +0530

    1stcommit

--- Main.py
+++ Main.py
@@ -1,25 +1,65 @@
 class MyCircularQueue:
     def __init__(self, size: int):
         # Write code here
+        self.queue = [0] * size
+        self.size = size
+        self.front = -1
+        self.rear = -1
 
     def enqueue(self, value: int) -> bool:
         # Write code here
-
+        if not self.is_full():
+            if self.front == -1:
+                self.front = 0
+                self.rear = 0            
+            else:
+                self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear]=value
+            return True
+        else:
+            return False
+            
+        
     def dequeue(self) -> bool:
         # Write code here
+        if not self.is_empty():            
+            if self.front == self.rear:
+                self.front = -1
+                self.rear = -1
+            else:
+                self.front = (self.front+1)%self.size
+            return True
+        else:
+            return False
+            
 
     def get_front(self) -> int:
         # Write code here
+        if not self.is_empty():
+            return self.queue[self.front]
+        else:
+            return -1
 
     def get_rear(self):
         # Write code here
+        if not self.is_empty():
+            return self.queue[self.rear]
+        else:
+            return -1
 
     def is_empty(self):
         # Write code here
+        if self.front == -1:
+            return True
+        else:
+            return False
 
     def is_full(self):
         # Write code here
-
+        if (self.front == 0 and self.rear == self.size-1) or self.front == (self.rear+1)%self.size:
+            return True
+        else:
+            return False
 
 # Do not change the following code
 operations = []
@@ -45,7 +85,7 @@ for i in range(len(operations)):
         result.append(obj.get_front())
     elif operations[i] == "dequeue":
         result.append(obj.dequeue())
-    elif operations[i] == "is_full":
+    elif operations[i] == "is_full": 
         result.append(obj.is_full())
     elif operations[i] == "is_empty":
         result.append(obj.is_empty())
